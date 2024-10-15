70f8f48c180ff379999e4a9288c30360f986f05e
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:14:02 +0530

    Add project files.

---
+++ ModifyingStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModifyingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Janani";
+            string name_updated = name.Insert(4, "ana");
+            Console.WriteLine(name_updated);
+
+            string name_updated2 = name.Remove(2, 4);
+            Console.WriteLine(name_updated2);
+            Console.ReadKey();
+        }
+    }
+}
